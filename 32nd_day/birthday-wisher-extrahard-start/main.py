d0572b5833093576168f897b7868dc5f2a1f109c
stanbesz <stanimiralexandrov@gmail.com>
2025-12-29 17:11:59 +0200

    32nd_day Email happy birthday

--- 32nd_day/birthday-wisher-extrahard-start/main.py
+++ 32nd_day/birthday-wisher-extrahard-start/main.py
@@ -3,40 +3,47 @@ import smtplib
 import datetime as dt
 import random as rd
 import pandas as pn
+from collections import defaultdict
+from pathlib import Path
 
-# 1. Update the birthdays.csv
+my_email = "stanbesz503@gmail.com"
+password = "ifel zjzp drno kdhk"
 
-# 2. Check if today matches a birthday in the birthdays.csv
 now_date = dt.datetime.now()
 now_day = now_date.day
 now_month = now_date.month
 now_year = now_date.year
-print(now_date)
 
 birthdays = pn.read_csv("32nd_day\\birthday-wisher-extrahard-start\\birthdays.csv")
 birthdays_df = pn.DataFrame(birthdays)
+birthday_map:dict = defaultdict(list)
 
-def check_birthday(now_day,now_month,birthdays_df):
+birthday_letters:list = []
 
-    result = False
-    name = ""
-    email = ""
-    for row in birthdays_df.itterrows():
-        if int(row.month) == now_month and int(row.day)==now_day:
-            result = True
-            name = row["name"]
-            email = row["email"]
-            print(f"email: f{email} | name: {name}")
+folder = Path("32nd_day\\birthday-wisher-extrahard-start\letter_templates")
 
+birthday_letters = [file.read_text(encoding="utf-8") for file in folder.glob("*.txt")]
 
-check_birthday(now_day,now_month,birthdays_df)
-print(birthdays_df)
+for _,row in birthdays_df.iterrows():
+    birthday_map[(row.day, row.month)].append((row["name"],row.email))
 
+def send_birthday_mail(name,email,birthday_letters):
+    birthday_letter = rd.choice(birthday_letters)
+    name_letter = birthday_letter.replace("[NAME]",name)
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+    with smtplib.SMTP("smtp.gmail.com") as connection: # Same as the file reading/writing
+        connection.starttls() #make connection secure
+        connection.login(user=my_email,password=password)
+        connection.sendmail(from_addr=my_email,to_addrs="stanbe_sz@abv.bg",msg=f"Subject: Happy Birthday {name} \n\n {name_letter}")
 
-# 4. Send the letter generated in step 3 to that person's email address.
+def send_emails(now_day,now_month,birthday_letters,birthday_map):
 
+    check_birthday = (now_day,now_month) in list(birthday_map.keys())
 
+    if check_birthday:
+        for person in birthday_map[(now_day,now_month)]:
+            send_birthday_mail(person[0],person[1],birthday_letters)
+    else:
+        print("No birthdays Today!")
 
-
+send_emails(now_day,now_month,birthday_letters,birthday_map)
